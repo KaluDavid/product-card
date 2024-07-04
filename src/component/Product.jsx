9729c7bdb7fd2cb6bf934fdb67a64e842c37a0db
Kalu David <kaludavidinyang@gmail.com>
2024-07-04 03:48:21 +0100

    completed project

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -5,25 +5,28 @@ import cart from "./images/icon-cart.svg";
 export function Product() {
   return (
     <>
-      <main>
-        <img src={img} alt="" className="img" />
-        <section>
-          <h3>Perfume</h3>
-          <h1>Gabrielle Essence Eau De Parfum</h1>
-          <article>
-            A floral, solar and voluptuous interpretation composed by Olivier
-            Polge, Perfumer-Creator for the House of CHANEL.
-          </article>
-          <div>
-            <p className="price1">$149.99</p>
-            <p className="price2">$169.99</p>
-          </div>
+      <body>
+        <main>
+          <img src={img} alt="" className="img" />
+          <section>
+            <h3>Perfume</h3>
+            <h1>Gabrielle Essence Eau De Parfum</h1>
+            <article>
+              A floral, solar and voluptuous interpretation composed by Olivier
+              Polge, Perfumer-Creator for the House of CHANEL.
+            </article>
+            
+            <div>
+              <p className="price1">$149.99</p>
+              <p className="price2">$169.99</p>
+            </div>
 
-          <button className="btn">
-            <img src={cart} alt="cart" /> Add to Cart
-          </button>
-        </section>
-      </main>
+            <button className="btn">
+              <img src={cart} alt="cart" /> Add to Cart
+            </button>
+          </section>
+        </main>
+      </body>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors:{
-
+    colors: {
       // Primary
 
-       'dark-cyan': 'hsl(158, 36%, 37%)',
-      'cream': 'hsl(30, 38%, 92%)',
-      
+      "dark-cyan": "hsl(158, 36%, 37%)",
+      cream: "hsl(30, 38%, 92%)",
+      zinc: "rgb(245 245 245)",
+
       // Neutral
-  
-      'very-dark-blue' : 'hsl(212, 21%, 14%)',
-      'dark-grayish-blue' : 'hsl(228, 12%, 48%)',
-      'White': 'hsl(0, 0%, 100%',
+
+      "very-dark-blue": "hsl(212, 21%, 14%)",
+      "dark-grayish-blue": "hsl(228, 12%, 48%)",
+      "pure-white": "hsl(0, 0%, 100%",
     },
     fontFamily: {
-      'fraunces' : ['Fraunces', 'serif'],
-      'montserrat' : ['Montserrat', 'sans-serif'],
-
+      fraunces: ["Fraunces", "serif"],
+      montserrat: ["Montserrat", "sans-serif"],
     },
+
+    screens: {
+      'mobile': '200px',
+      'tablet': '768px',
+      'laptop': '1024px',
+      'xlaptop': '1280px',
+      'desktop': '1440px',
+    },
+
+    boxShadow: {
+      '3xl': '5px 5px 5px 0 rgba(0, 0, 0, 0.3)',
+      '2xl': '0 5px 5px 0 rgba(0, 0, 0, 0.3)',
+      'none': '0 0 #0000',
+    },
+
     extend: {},
   },
   plugins: [],
-}
+};
